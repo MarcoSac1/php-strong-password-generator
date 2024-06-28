c5a732dbc183ca9a19ed4efeb1ae1acc3cf6fb97
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-28 18:26:22 +0200

    moved arrai and form in login.php

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <?php
-$utenti =[
-    [
-        'username' => 'franco',
-        'password' => 'calippo'
-    ],
-    [
-        'username' => 'erminio',
-        'password' => 'antani'
-    ],
-    [
-        'username' => 'pippo',
-        'password' => 'panti'
-    ],
-    [
-        'username' => 'enza',
-        'password' => 'saturo'
-    ],
-    [
-        'username' => 'antonio',
-        'password' => 'giumenta'
-    ],
-];
 session_start();
 
-var_dump($_SESSION);
+
 ?>
 
 <!DOCTYPE html>
@@ -36,8 +14,8 @@ var_dump($_SESSION);
 </head>
 </head>
 <body>
-    <header>
-        <ul>
+    <header >
+        <ul class="d-flex justify-content-between">
             <li>
                 <a href="./index.php">Home</a>
             </li>
@@ -49,25 +27,6 @@ var_dump($_SESSION);
             </li>
         </ul>
     </header>
-    <main>
-        <form action="./login.php" method="$_GET">
-            <label for="username">create your username:</label>
-            <input type="text" name="username" id="username">
-            <label for="password">create your password:</label>
-            <input type="text" name="password" id="password">
-            <button type="submit">Login</button>
-        </form>
-        <ul class="  w-25">
-        <?php foreach($utenti as $user ) {?>
-            <li class="bg-warning ">
-                <p>nome: <?php echo $user['username'];?></p>
-            </li>
-            <li class="bg-danger text-white mb-2">
-                <p>password: <?php echo $user['password'];?></p>
-            </li>
-            <?php }?>
-        </ul>
-    </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
